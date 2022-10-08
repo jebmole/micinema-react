db3c981108421d2d9b3827157428f73f42ca62fe
Jonathan Estrada <jestradabe@gmail.com>
2022-10-08 13:08:17 -0500

    Implementacion inicial de cinema ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Carousel from "./components/Carousel";
+import Navbar from "./components/Navbar";
 
 function App() {
+
+  const [recentMovies, setRecentMovies] = useState([]);
+
+  useEffect(()=>{
+
+    const getMoviesAsync = async () => {
+    
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=a6db559033af943be136a2110dbd4b5f&language=es-CO", {
+        method: "GET",
+      });
+  
+      const jsonData = await response.json();
+      setRecentMovies(jsonData.results);
+    }
+
+    //Este codigo se ejecuta cada vez que se renderiza el componente
+    getMoviesAsync();
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Carousel movies={recentMovies.slice(0, 3)} />
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,65 @@
+import imagen1 from "../images/imagen1.png";
+
+function Carousel({ movies }) {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        {movies.map((movie, index) => {
+          return (
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to={index}
+              className={index === 0 ? "active" : ""}
+              aria-current="true"
+              aria-label={`Slide ${index + 1}`}
+            ></button>
+          );
+        })}
+      </div>
+      <div className="carousel-inner">
+        {movies.map((movie, index) => {
+          return (
+            <div className={ index === 0 ? "carousel-item active" : "carousel-item"} data-bs-interval="2000">
+              <img
+                src={`http://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+                className="d-block w-100"
+                alt={movie.title}
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>{movie.title}</h5>
+                <p>
+                  {movie.overview}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;
